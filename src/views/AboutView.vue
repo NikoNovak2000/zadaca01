65b507c8bc99494ddcf0873e63d8a84b69079bf7
Niko Novak <nnovak@unipu.hr>
2022-03-24 21:41:48 +0100

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    {{ ime }}
+    {{ sha }}
+    <router-view />
   </div>
 </template>
+<script>
+export default {
+  props:["sha"],
+  data() {
+    return {
+      ime: "OK"
+    };
+  },
+  name: "AboutPage",
+};
+</script>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <ul>
+
+      </ul>
+
+    <ul>
+      <!-- u key se stavlja nešto jedinstveno - primary key -->
+      <li v-for="item in commits" v-bind:key="item.sha"> Commit:  <router-link :to="'/commit/' + item.sha">{{ item.sha }} </router-link> </li>
+      </ul>
+
   </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+// JS Objekt -> ograničen vitičastim {} zagradama -> koji je definicija Vue komponente
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: 'HomeView', //string
+  data() {
+   return {
+     // mora vratiti objekt ciji atributi (ime,prezime) su vidljivi HTML template dijelu
+     ime: "Niko",
+     prezime: "Novak",
+     commits: [],
+   };
+  },
+  async mounted(){  //izvršava se kad se komponenta pokreće (funkcija)
+
+              //dohvatamo postove s githuba -> na github adresu
+  let rezultat = await fetch("http://localhost:9999/github.json"); 
+
+  let podaci = await rezultat.json()
+
+  // console.log(typeof podaci); //vrsta podataka (array je zbog tip zagrade{} )
+
+  // of - za array
+  // in - za objekt
+  for (let item of podaci) {
+    item:
+    console.log(item.sha);
   }
-}
+  
+
+  //mora se koristit this da bi se pristupio data
+  this.commits = podaci;
+  },
+};
+//Vue.js je poziva
+
 </script>
